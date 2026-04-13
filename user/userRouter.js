27f4731bf7633b46cccd990a6d76c7ef7fe2c9a5
yeajin458 <yeajin040508@naver.com>
2026-04-13 13:39:41 +0900

    회원 기능 수정

--- user/userRouter.js
+++ user/userRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router(); //라우팅 객체를 만듦
-const user = require("../user.js"); //함수 호출
+const user = require("./user.js"); //함수 호출
 
 router.post("/auth/signup", user.signup); //회원가입
 router.post("/auth/login", user.login); //로그인
