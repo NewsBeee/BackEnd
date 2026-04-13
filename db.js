3af81a375780780ea1469f05068285a31e041808
yeajin458 <yeajin040508@naver.com>
2026-04-13 13:15:07 +0900

    db 연결

---
+++ db.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var mysql = require("mysql2");
+
+var db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("DB 연결 실패:", err);
+    return;
+  }
+  console.log("DB 연결 성공");
+  connection.release();
+});
+
+module.exports = db;
