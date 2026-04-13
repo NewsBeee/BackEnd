ba4ca0a02c47b2d9612cd9b917b2fe35c0b66a3e
yeajin458 <yeajin040508@naver.com>
2026-04-13 13:19:41 +0900

    db 연결

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ var db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   multipleStatements: true,
-});
+}).promise;
 
 db.getConnection((err, connection) => {
   if (err) {
