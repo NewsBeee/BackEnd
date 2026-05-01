2e3fadc75d1131ed174ee1a1eb2f5dd92d670590
yeajin458 <yeajin040508@naver.com>
2026-05-01 15:37:05 +0900

    기사 변환 라우터

--- article/articleRouter.js
+++ article/articleRouter.js
@@ -4,4 +4,9 @@ const article = require("./article");
 
 router.get("/recommendations/articles", article.recommendations);
 
+router.post("/articles/transform", article.transform);
+router.post("/articles/transform/{articleId}/read", article.read);
+router.get("/articles", article.article);
+router.get("/articles/{articleId}", article.articlesdetail);
+router.get("/articles/guest/quota", article.guest);
 module.exports = router;
