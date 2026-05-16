85f7b62e636ea5134b1eed0b799c18646f60543d
yeajin458 <yeajin040508@naver.com>
2026-05-16 22:00:25 +0900

    오타  수정

--- article/articleRouter.js
+++ article/articleRouter.js
@@ -5,7 +5,7 @@ const article = require("./article");
 router.get("/recommendations/articles", article.recommendations);
 router.get("/articles/guest/quota", article.getGuestQuota);
 router.post("/articles/transform", article.transform);
-router.post("/articles/transform/:articleId/read", article.readinglog);
+router.post("/articles/:articleId/read", article.readinglog);
 router.get("/articles", article.articlelist);
 router.get("/articles/:articleId", article.articledetail);
 
